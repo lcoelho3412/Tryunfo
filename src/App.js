b6c7164ea5d0a1b48fe0792a58af978696bee9f3
lcoelho3412 <leandrocoelho3@yahoo.com>
2022-07-05 17:51:55 -0300

    Requisito 9

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends React.Component {
       cardRare: '',
       cardTrunfo: false,
       hasTrunfo: false,
-      cards: [newCard, ...prevState.cards],
+      cards: [...prevState.cards, newCard],
     }));
   };
 
